7454b3fc9518e7d0ecc1361b3a9d418ca6ac07cf
PasitGut <pasitgut.dev@gmail.com>
2026-03-02 18:47:58 +0700

    fix index.html

--- apps/lab08-ai/lab08-ai/vite.config.ts
+++ apps/lab08-ai/lab08-ai/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mobileweb/lab08/",
   plugins: [
     vue(),
     legacy()
--- apps/lab09-sensors/vite.config.ts
+++ apps/lab09-sensors/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mobileweb/lab09/",
   plugins: [
     vue(),
     legacy()
